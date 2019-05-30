3d5faeb6db4452764be45ef5abc8e8a67b725e9f
BE\bdevezademacedoesilv <bdevezademacedoesilv@deloitte.com>
2019-05-30 12:08:20 +0200

    exercice 1 question

---
+++ exercice.js
@@ -0,0 +1,8 @@
+/* 
+Write a function that removes repeated words from a string.
+example
+str = "this has has repeated repeated words words"
+str = "this has repeated words"
+*/
+
+/* P.S ; add a new branch called feature/exercice1 and commit /push the solution on this branch. good luck*/
